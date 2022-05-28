e7fd40eaa99632d81acd7af35d59e357c0450c4c
aawesh2000 <aawesh2000@gmail.com>
2022-05-28 11:59:25 +0530

    Updates

---
+++ precache-manifest.3959e3b6dd2ca647a64366e57cd9be66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8210b9e5ba67d8c0753291b538485431",
+    "url": "/navbar-dropdown-v1/index.html"
+  },
+  {
+    "revision": "e290831c81d3b9e4e724",
+    "url": "/navbar-dropdown-v1/static/css/main.cca13aff.chunk.css"
+  },
+  {
+    "revision": "985b50a94e505788b0eb",
+    "url": "/navbar-dropdown-v1/static/js/2.599373e5.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/navbar-dropdown-v1/static/js/2.599373e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e290831c81d3b9e4e724",
+    "url": "/navbar-dropdown-v1/static/js/main.a8aa3d2a.chunk.js"
+  },
+  {
+    "revision": "a47161cb486ba2dd4801",
+    "url": "/navbar-dropdown-v1/static/js/runtime-main.fc7893b3.js"
+  }
+]);
